7f8e23550218848ff9fc520c89f57864fdfd57af
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-08 13:10:42 +0000

    Reset to c0b9eb12fcd82d306ff35ca60ea4d12ef392f676 and made changes for LLM Round 1

--- tests/test_paginate.py
+++ tests/test_paginate.py
@@ -6,27 +6,8 @@ from moffee.compositor import composite, Direction, Type
 def sample_document():
     return \
 """
----
-resource_dir: resources
-default_h1: true
-default_h2: false
-layout: split
-theme: beam
-background-color: 'red'
----"
-# Main Title
-
-Other Pages
-![Image-1](image.png)
----
-Paragraph 1
-===
-Paragraph 2
-<->
-Paragraph 3
-<->
-![Image-2](image2.png)
-"""
+---resource_dir: resources\ndefault_h1: true\ndefault_h2: false\nlayout: split\ntheme: beam\nbackground-color: 'red'\n---"
+# Main Title\n\nOther Pages\n![Image-1](image.png)\n---\nParagraph 1\n===\nParagraph 2\n<->\nParagraph 3\n<->\n![Image-2](image2.png)"
 
 
 def test_paginate_creates_correct_number_of_pages(sample_document):
@@ -39,13 +20,13 @@ def test_frontmatter_parsing(sample_document):
     assert pages[0].option.layout == 'split', 'Expected layout to be split'
     assert pages[0].option.default_h1 is True, 'Expected default_h1 to be True'
     assert pages[0].option.default_h2 is False, 'Expected default_h2 to be False'
-    assert pages[0].option.styles == {'background-color': 'red'}, 'Expected background color to be 'red''
+    assert pages[0].option.styles == {'background-color': 'red'}, 'Expected background color to be red'
 
 
 def test_style_overwrite(sample_document):
     pages = composite(sample_document)
-    assert pages[1].option.styles == {'background-color': 'yellow'}, 'Expected background color to be 'yellow'
-    assert pages[0].option.styles == {'background-color': 'red'}, 'Expected original background color to be 'red'
+    assert pages[1].option.styles == {'background-color': 'yellow'}, 'Expected background color to be yellow'
+    assert pages[0].option.styles == {'background-color': 'red'}, 'Expected original background color to be red'
 
 # Add more test functions as needed to cover other scenarios
 
