516c0ee97342ebdca504f5b96623556a16cc2d85
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 16:58:10 +0000

    Reset to c0b9eb12fcd82d306ff35ca60ea4d12ef392f676 and made changes for LLM Round 1

--- tests/test_paginate.py
+++ tests/test_paginate.py
@@ -54,27 +54,27 @@ This is line 12.
 """
 
 
-def test_paginate_creates_correct_number_of_pages(sample_document):
-    pages = composite(sample_document)
+def test_paginate_creates_correct_number_of_pages():
+    pages = composite(sample_document())
     assert len(pages) > 1, "Pagination should create multiple pages"
 
 
-def test_frontmatter_parsing(sample_document):
-    pages = composite(sample_document)
+def test_frontmatter_parsing():
+    pages = composite(sample_document())
     assert pages[0].option.layout == "split"
     assert pages[0].option.default_h1 is True
     assert pages[0].option.default_h2 is False
     assert pages[0].option.styles == {"background-color": "gray"}
 
 
-def test_style_overwrite(sample_document):
-    pages = composite(sample_document)
+def test_style_overwrite():
+    pages = composite(sample_document())
     assert pages[1].option.styles == {"background-color": "yellow"}
     assert pages[0].option.styles == {"background-color": "gray"}
 
 
-def test_header_inheritance(sample_document):
-    pages = composite(sample_document)
+def test_header_inheritance():
+    pages = composite(sample_document())
     assert pages[0].h1 == "Main Title"
     assert pages[1].h1 is None
     assert pages[1].h2 == "Subtitle"
@@ -83,31 +83,31 @@ def test_header_inheritance(sample_document):
     assert pages[2].h3 == "Subheader"
 
 
-def test_page_splitting_on_headers(sample_document):
-    pages = composite(sample_document)
+def test_page_splitting_on_headers():
+    pages = composite(sample_document())
     assert len(pages) == 3
     assert pages[0].h1 == "Header 1"
     assert pages[1].h2 == "Header 2"
     assert pages[2].h1 == "New Header 1"
 
 
-def test_page_splitting_on_dividers(sample_document):
-    pages = composite(sample_document)
+def test_page_splitting_on_dividers():
+    pages = composite(sample_document())
     assert len(pages) == 2
 
 
-def test_escaped_area_paging(sample_document):
-    pages = composite(sample_document)
+def test_escaped_area_paging():
+    pages = composite(sample_document())
     assert len(pages) == 1
 
 
-def test_escaped_area_chunking(sample_document):
-    pages = composite(sample_document)
+def test_escaped_area_chunking():
+    pages = composite(sample_document())
     assert len(pages[1].chunk.children) == 0
 
 
-def test_chunking_horizontal(sample_document):
-    pages = composite(sample_document)
+def test_chunking_horizontal():
+    pages = composite(sample_document())
     chunk = pages[0].chunk
     assert chunk.type == Type.NODE
     assert len(chunk.children) == 3
@@ -115,8 +115,8 @@ def test_chunking_horizontal(sample_document):
     assert chunk.children[0].type == Type.PARAGRAPH
 
 
-def test_chunking_hybrid(sample_document):
-    pages = composite(sample_document)
+def test_chunking_hybrid():
+    pages = composite(sample_document())
     assert len(pages) == 2
     chunk = pages[1].chunk
     assert chunk.type == Type.NODE
@@ -130,21 +130,21 @@ def test_chunking_hybrid(sample_document):
     assert len(next_chunk.children) == 3
 
 
-def test_empty_lines_handling(sample_document):
-    pages = composite(sample_document)
+def test_empty_lines_handling():
+    pages = composite(sample_document())
     assert len(pages[0].chunk.children) == 0
     assert pages[0].option.styles == {}
 
 
-def test_deco_handling(sample_document):
-    pages = composite(sample_document)
+def test_deco_handling():
+    pages = composite(sample_document())
     assert pages[0].raw_md == "Hello"
     assert pages[0].option.default_h1 is False
     assert pages[0].option.styles == {"background": "blue"}
 
 
-def test_multiple_deco(sample_document):
-    pages = composite(sample_document)
+def test_multiple_deco():
+    pages = composite(sample_document())
     assert len(pages) == 2
     assert pages[0].raw_md == ""
     assert pages[0].title == "Title1"
