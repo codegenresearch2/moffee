1c9b4984f2ed39da636fa85d23c324c434fe193a
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 11:07:01 +0000

    Reset to c0b9eb12fcd82d306ff35ca60ea4d12ef392f676 and made changes for LLM Round 1

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -12,25 +12,7 @@ def template_dir(name="base"):
 
 @pytest.fixture(scope="module", autouse=True)
 def setup_test_env():
-    doc = """
----
-resource_dir: "resources"
-default_h1: true
-theme: beam
-background-color: 'red'
----
-# Test page
-Other Pages
-![Image-1](image.png)
----
-Paragraph 1
-===
-Paragraph 2
-<->
-Paragraph 3
-<->
-![Image-2](image2.png)
-    """
+    doc = """\n---\nresource_dir: "resources"\ndefault_h1: true\ntheme: beam\nbackground-color: 'red'\n---\n# Test page\nOther Pages\n![Image-1](image.png)\n---\nParagraph 1\n___\nParagraph 2\n***\nParagraph 3\n***\n![Image-2](image2.png)\n    """
     with tempfile.TemporaryDirectory() as temp_dir:
         # Setup test files and directories
         doc_path = os.path.join(temp_dir, "test.md")
@@ -68,8 +50,6 @@ def test_rendering(setup_test_env):
 
 def test_read_options(setup_test_env):
     _, doc_path, _, _ = setup_test_env
-    # import ipdb; ipdb.set_trace(context=15)
-
     options = read_options(doc_path)
     assert options.default_h1 is True
     assert options.theme == "beam"
@@ -100,19 +80,7 @@ def test_build(setup_test_env):
 
 
 def test_retrieve_structure():
-    doc = """
-# Title
-p0
-## Heading1
-p1
-### Subheading1
-p2
-## Heading2
-### Subheading1
-p3
-# Title2
-p4
-"""
+    doc = """\n# Title\np0\n## Heading1\np1\n### Subheading1\np2\n## Heading2\n### Subheading1\np3\n# Title2\np4\n"""
     pages = composite(doc)
     slide_struct = retrieve_structure(pages)
     headings = slide_struct["headings"]
